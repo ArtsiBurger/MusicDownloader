d46226fffedaf1b63b43de7ba0192d92f1a076d6
parssinen.arthur@gmail.com <parssinen.arthur@gmail.com>
2025-10-30 10:06:31 +0200

    Adjusted folders for mobile

--- user_inputs.py
+++ user_inputs.py
@@ -6,7 +6,7 @@ def get_user_inputs():
     if not spotify_url:
         spotify_url ="https://open.spotify.com/playlist/66Z07tOKFQrzTnhtTtEPsL?si=1cd350c11e3a4ba6&pt=2982a41f8fb67b1b33e6537d558170af" # Dummy playlist for testing
     
-    base_path = r"C:\Users\pc\Music"
+    base_path = r"C:\Users\pc\Music" # For PC: r"C:\Users\pc\Music" | For mobile: "/storage/emulated/0/Music"
     user_folder = input("Give folder name inside Music-folder where the MP3s will be saved to: ")
     if not user_folder:
         user_folder = "ConvertedMP3s"
