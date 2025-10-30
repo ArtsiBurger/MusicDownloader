e08b4530e663c5fe7fdce0a61ff5c920c56dcf4f
parssinen.arthur@gmail.com <parssinen.arthur@gmail.com>
2025-10-30 21:22:33 +0200

    changed the test folder

--- PlaylistSelector/scripts/switch_playlist.py
+++ PlaylistSelector/scripts/switch_playlist.py
@@ -1,5 +1,5 @@
 def disable_playlist():
-    folder = "test"
+    folder = "LDN"
     open(f"/storage/emulated/0/Music/{folder}/.nomedia", "a").close()
     print(f"Folder {folder} is disabled.")
-    
+
