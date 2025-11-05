f482ed9fd9eeb525feeadce3fb89a05cc8a36485
parssinen.arthur@gmail.com <parssinen.arthur@gmail.com>
2025-11-05 12:21:14 +0200

    kivy practice

--- KivyUI/main.py
+++ KivyUI/main.py
@@ -1,9 +1,19 @@
 from kivy.app import App
-from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import ListProperty
 
-class MyApp(App):
+class ColorButton(BoxLayout):
+    color = ListProperty([1, 0, 0, 1])
+
+    def change_color(self):
+        if self.color == [1, 0, 0, 1]:
+            self.color = [0, 0, 1, 1]
+        else:
+            self.color = [1, 0, 0, 1]   
+
+class ColorApp(App):
     def build(self):
-        return Label(text="Hello, Kivy!")
-
+        return ColorButton()
+    
 if __name__ == "__main__":
-    MyApp().run()
+    ColorApp().run()
