d723cd3b7e76da762028bfe11fd7aa8a3a53d6d5
parssinen.arthur@gmail.com <parssinen.arthur@gmail.com>
2025-11-06 12:29:56 +0200

    base for the app

---
+++ KivyUI/main.py
@@ -0,0 +1,11 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+
+class PlaylistScreen(BoxLayout):
+    pass
+
+class MusicApp(App):
+    pass
+
+if __name__ == "__main__":
+    MusicApp().run()
