387aaed9590f7522b5743ed4a423d9229e4f3559
parssinen.arthur@gmail.com <parssinen.arthur@gmail.com>
2025-11-06 10:56:04 +0200

    kivy practice

---
+++ KivyUI/KivyPractice/counter.py
@@ -0,0 +1,13 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import ListProperty
+
+class CounterButton(BoxLayout):
+   color = ListProperty([1, 1, 1, 1])
+
+class CounterApp(App):
+    def build(self):
+        return CounterButton()
+    
+if __name__ == "__main__":
+    CounterApp().run()
