28bbed0af75a3ccecc5a81c730fb27b0d68f44a1
parssinen.arthur@gmail.com <parssinen.arthur@gmail.com>
2025-11-06 13:04:08 +0200

    making the UI

--- KivyUI/main.py
+++ KivyUI/main.py
@@ -1,11 +1,28 @@
+import os
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import ListProperty, StringProperty
 
 class PlaylistScreen(BoxLayout):
-    pass
+    base_path = StringProperty(r"C:\Users\pc\Music") # For PC: r"C:\Users\pc\Music" | For mobile: "/storage/emulated/0/Music"
+    playlists = ListProperty([])
+
+    def __init__(self, **kwargs): # Auto load playlists on start
+        super().__init__(**kwargs)
+        self.load_playlists()
+
+    def load_playlists(self):
+        self.playlists.clear()
+        try:
+            for f in os.listdir(self.base_path): # Sort out only the folders in the base path
+                if os.path.isdir(os.path.join(self.base_path, f)):
+                    self.playlists.append(f)
+        except Exception as e:
+            print(f"Error loading playlists: {e}")
 
 class MusicApp(App):
-    pass
+    def build(self):
+        return PlaylistScreen()
 
 if __name__ == "__main__":
     MusicApp().run()
